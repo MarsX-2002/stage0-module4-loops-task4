321b1a6438b2d3167ac331f768d716c49f7d95b5
MarsX-2002 <mirjalol0331@gmail.com>
2023-04-03 20:18:45 +0500

    break, continue

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -4,7 +4,7 @@ public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
         if (numberToGoUntil < toBreakWith) {
             System.out.println("iterating till the end");
-            for (int i = 1; i < numberToGoUntil; i++) {
+            for (int i = 1; i <= numberToGoUntil; i++) {
                 System.out.println(i);
             }
         } else {
@@ -13,5 +13,11 @@ public class NumberToBreakOn {
             }
         }
 
+
+        for (int i =1;i<=numberToGoUntil;i++){
+            if (numberToGoUntil<toBreakWith){
+                System.out.println();
+            }
+        }
     }
 }
